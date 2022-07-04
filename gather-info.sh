cf119c1fdccc130d2db768d54cd19005ad27afdf
Julia <kuznetsovajulia@gmail.com>
2022-07-04 14:47:48 +0300

    Adding Bash files

--- gather-info.sh
+++ gather-info.sh
@@ -1,17 +1,11 @@
-#!/bin/bash	
-echo "Starting at: $(date)"	
-echo	
-	
-echo "UPTIME"	
-uptime	
-echo	
-	
-echo "FREE"	
-free	
-echo	
-	
-echo "WHO"	
-who	
-echo	
-	
-echo "Finishing at: $(date)"	
+#!/bin/bash
+line="--------------------------------------"
+echo "Starting at: $(date)"; echo $line
+
+echo "UPTIME"; uptime; echo $line
+
+echo "FREE"; free; echo $line
+
+echo "WHO"; who; echo $line
+
+echo "Finishing at: $(date)"
